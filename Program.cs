31a9a6eb3029e044657c4fb480eb7f62f8f65041
Jorge Ariel Garrone <jorge.garrone@gmail.com>
2022-06-09 12:36:02 -0400

    Add: add connection string to SQL server

--- Program.cs
+++ Program.cs
@@ -1,6 +1,21 @@
+using EF_Fundamentals.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
+
+//builder.Services.AddDbContext<TaskContext>(p => p.UseInMemoryDatabase("TaskDb"));
+builder.Services.AddSqlServer<TaskContext>(builder.Configuration.GetConnectionString("taskConn"));
+
+
 var app = builder.Build();
 
 app.MapGet("/", () => "Hello World!");
+
+app.MapGet("/dbconnection", async ([FromServices] TaskContext dbContext ) =>
+{
+    dbContext.Database.EnsureCreated();
+    return Results.Ok("Database in memory: " + dbContext.Database.IsInMemory());
+});
 
 app.Run();
